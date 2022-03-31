95e5a0ea706336598ceb42232711dfbcaf7cd277
edodima13 <edodima13@libero.it>
2022-03-31 19:58:05 +0200

    avanzamento

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+//creare un array con domanda e risposte
+$faq = [
+    'd' => 'Come state implementando la recente decisione della Corte di giustizia dell \'Unione europea (CGUE) relativa al diritto all \'oblio?'
+    'r' => 'La recente decisione della Corte di giustizia dell \'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.'
+];
+//fare un ciclo for each con i contenuti dell'array
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+</body>
+</html>
